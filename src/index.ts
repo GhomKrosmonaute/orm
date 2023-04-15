7d4ea4895c561e493224c1c5e8b8b712432c9a07
GhomKrosmonaute <camille.abella@hotmail.fr>
2023-04-15 14:57:31 +0200

    added declarations for sub modules

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
 export * from "./app/orm.js"
 export * from "./app/table.js"
+
+// @ts-ignore
+declare module "@ghom/orm/esm" {
+  export * from "./app/orm.js"
+  export * from "./app/table.js"
+}
+
+// @ts-ignore
+declare module "@ghom/orm/cjs" {
+  export * from "./app/orm.js"
+  export * from "./app/table.js"
+}
